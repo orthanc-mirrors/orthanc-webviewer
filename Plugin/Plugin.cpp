c5095127346ce31b5e51fde3702afb1de413e4db
Sebastien Jodogne <s.jodogne@gmail.com>
2020-03-13 14:03:23 +0100

    fix compilation with Holy Build Box (for LSB)

--- Plugin/Plugin.cpp
+++ Plugin/Plugin.cpp
@@ -36,6 +36,25 @@
 #include <boost/filesystem.hpp>
 
 
+/**
+ * We force the redefinition of the "ORTHANC_PLUGINS_API" macro, that
+ * was left empty with gcc until Orthanc SDK 1.5.7 (no "default"
+ * visibility). This causes the version script, if run from "Holy
+ * Build Box", to make private the 4 global functions of the plugin.
+ **/
+
+#undef ORTHANC_PLUGINS_API
+
+#ifdef WIN32
+#  define ORTHANC_PLUGINS_API __declspec(dllexport)
+#elif __GNUC__ >= 4
+#  define ORTHANC_PLUGINS_API __attribute__ ((visibility ("default")))
+#else
+#  define ORTHANC_PLUGINS_API
+#endif
+
+
+
 static OrthancPluginContext* context_ = NULL;
 static bool restrictTransferSyntaxes_ = false;
 static std::set<std::string> enabledTransferSyntaxes_;
